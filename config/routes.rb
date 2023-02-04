05258e9df97c29a93d879549c03837d0557ede79
virgostyx <virgostyx@gmail.com>
2023-02-04 17:48:29 +0000

    Finish to copy all source code

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,10 @@
 Surveyor::Engine.routes.draw do
+
+  get '/', to: 'surveyor#new', as: 'available_surveys'
+  post '/:survey_code', to: 'surveyor#create', as: 'take_survey'
+  get '/:survey_code', to: 'surveyor#export', as: 'export_survey'
+  get '/:survey_code/:response_set_code', to: 'surveyor#show', as: 'view_my_survey'
+  get '/:survey_code/:response_set_code/take', to: 'surveyor#edit', as: 'edit_my_survey'
+  put '/:survey_code/:response_set_code', to: 'surveyor#update', as: 'update_my_survey'
+
 end
